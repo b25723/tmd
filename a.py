2ec7498baf1290eaf47798a620d968a73a9e9967
b25723 <b25723@hotmail.com>
2017-08-30 01:55:39 +0800

    deleted:    a.py

--- a.py
+++
@@ -1,8 +0,0 @@
-import paramiko
-import time
-
-ssh=paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('10.24.254.50',username='root',password='aA1 12345')
-stdin,stdout,stderr=ssh.exec_command('get_ems')
-print stdout.read()
